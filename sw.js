8715803cc3bd067752ab0f1b813b5687ba897ee6
egistore344-droid <egistore344@gmail.com>
2026-09-26 11:47:35 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker Kedai Jajanan AMEERA
 // Ganti angka versi ini setiap kali mengubah file supaya aplikasi di HP ikut terbarui.
-const VERSI = "ameera-v17";
-const FILE = ["./", "./index.html", "./kasir.html", "./menu.js?v=17", "./toko.js?v=17", "./kasir.json", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable.png"];
+const VERSI = "ameera-v18";
+const FILE = ["./", "./index.html", "./kasir.html", "./menu.js?v=18", "./toko.js?v=18", "./kasir.json", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(VERSI).then(c => c.addAll(FILE)));
